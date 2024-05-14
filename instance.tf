425626c942832172347ad4e839d232bdfb6e6984
iikier <ivanikier@hotmail.com>
2024-05-13 21:59:13 -0300

    user data

--- instance.tf
+++ instance.tf
@@ -8,19 +8,35 @@ resource "aws_instance" "minikube" {
     Name = "Minikube-Instance"
   }
 
-  provisioner "file" {
-    source      = "web.sh"
-    destination = "/tmp/web.sh"
-  }
+  user_data = <<-EOF
+              sudo apt-get update -y
+              sudo apt-get install ca-certificates curl -y
+              sudo install -m 0755 -d /etc/apt/keyrings
+              sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+              sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-  provisioner "remote-exec" {
+              # Add the repository to Apt sources:
+              echo \
+              "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+              $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+              sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt-get update -y
 
-    inline = [
-      "chmod +x /tmp/web.sh",
-      "sudo /tmp/web.sh"
-    ]
-  }
+              sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+              # Instala o Minikube
+              sudo apt-get install -y curl
+              curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+              # Inicia o Minikube
+              sudo minikube start --driver=none
+              # Instala o kubectl
+              snap install kubectl --classic
+              # Implantar o Nginx
+              kubectl create deployment nginx --image=nginx
+              kubectl expose deployment nginx --port=80 --type=NodePort
+              EOF
 }
+
 output "PublicIP" {
   value = aws_instance.minikube.public_ip
 }
